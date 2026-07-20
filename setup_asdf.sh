2ff99d8cb98ce1250e392fe9cff38ff2503a10ac
Thiago F. Dias <t.dias@mshimizu.com.br>
2026-07-20 14:12:49 -0300

    feat: enable asdf plugin install script and add dotnet, bun

--- setup_asdf.sh
+++ setup_asdf.sh
@@ -1,46 +1,38 @@
 # #!/bin/bash
 
-# export ASFD_DOWNLOAD_URL="https://github.com/asdf-vm/asdf/releases/download/v0.18.0/asdf-v0.18.0-linux-amd64.tar.gz"
 export GITHUB_USERNAME="Thiago-Fernandes-Dias"
 
-# cd ~/Downloads
+ASDF_PLUGINS=(
+	chezmoi python nodejs java maven dotnet bun
+)
 
-# wget -O asdf.tar.gz $ASFD_DOWNLOAD_URL
+for i in "${ASDF_PLUGINS[@]}"
+do
+    echo "Installing asdf plugin $i -------------------------"
+    asdf plugin add "$i"
+    echo "asdf plugin $i installation finished --------------"
+done
 
-# tar -xvzf asdf.tar.gz
+asdf install java openjdk-21
+asdf set --home java openjdk-21
 
-# mkdir -p ~/.local/bin
+asdf install python 3.12.7
+asdf set --home python 3.12.7
 
-# mv asdf ~/.local/bin
+asdf install nodejs latest
+asdf set --home nodejs latest
 
-# rm asdf.tar.gz
+asdf install chezmoi latest
+asdf set --home chezmoi latest
 
+asdf install maven latest
+asdf set --home maven latest
 
-# ASDF_PLUGINS=(
-# 	chezmoi python nodejs java maven
-# )
+asdf install dotnet latest
+asdf set --home dotnet latest
 
-# for i in "${ASDF_PLUGINS[@]}"
-# do
-#     echo "Installing asdf plugin $i -------------------------"
-#     asdf plugin add "$i"
-#     echo "asdf plugin $i installation finished --------------"
-# done
-
-# asdf install java openjdk-21
-# asdf set --home java openjdk-21
-
-# asdf install python 3.12.7
-# asdf set --home python 3.12.7
-
-# asdf install nodejs latest
-# asdf set --home nodejs latest
-
-# asdf install chezmoi latest
-# asdf set --home chezmoi latest
-
-# asdf install maven latest
-# asdf set --home maven latest
+asdf install bun latest
+asdf set --home bun latest
 
 export PATH=$PATH:$HOME/.local/bin
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
